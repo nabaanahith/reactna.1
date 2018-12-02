171b940252c8b3a49c9cf27b080dac7c075176b9
nabaan <nabaa96@DOMAIN.local>
2018-12-02 18:06:16 +0300

    edited

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View, StyleSheet} from 'react-native';
 import Header from '../componenet/header';
 import Search from '../componenet/search';
 import List from '../componenet/list';
@@ -9,13 +9,26 @@ import {Constants} from 'expo';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex:1,marginTop:Constants.statusBarHeight}}>
+      <View style={styles.container}>
+ 
       <Header/>
-      <View style={{height:1,backgroundColor:'#2C2C2C'}}>
+      <View style={styles.paragraph}>
 </View>
       <List/>
       </View>
     );
+    
   }
 }
+    const styles = StyleSheet.create({
+      container: {
+        flex:1,
+        marginTop:Constants.statusBarHeigh
+      },
+      paragraph: {
+        height:1,
+        backgroundColor:'#2C2C2C'
+      },
+    });
+
 
